46fec2adcf3d28e2208ac381290442b166be969c
srkds <im.nirav86@gmail.com>
2026-01-17 19:50:19 +0530

    add sphinx doc

---
+++ docs/build/html/_sources/api.rst.txt
@@ -0,0 +1,17 @@
+API Documentation
+===================
+
+When you design a neural network each layer consists of no of nurons and single nuron is made up of ``linear->activation`` functions. Following functions are atomic independent functions to create a neural net.
+
+
+1. Linear Layer
+
+   .. autofunction:: nn.linear
+
+2. Linear Backward
+
+   .. autofunction:: nn.linear_backward
+
+3. Activation
+
+   .. autofunction:: nn.activation
---
+++ docs/source/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import pathlib
+import sys
+import os
+sys.path.insert(0, os.path.abspath('../../Improve-Deep-NN/NN/src'))
+
+project = 'Deep Learning From Scratch'
+copyright = '2026, Nirav'
+author = 'Nirav'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+        "sphinx.ext.autodoc",
+        "sphinx.ext.napoleon"
+        ]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# for deployment
+
+html_baseurl = "https://USERNAME.github.io/REPO/"
+
+extensions += ["sphinx.ext.githubpages"]
